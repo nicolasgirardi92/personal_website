9890ec638814107bdeea8885ac6cf630e8110cb7
Nicolas Girardi <nicolasgirardi@yahoo.com.ar>
2025-06-02 15:11:50 -0300

    first commit

---
+++ src/app/no_page/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function no_page() {
+    return (
+        <div className="page">
+            <h1>
+                Página en construcción
+            </h1>
+            <Image src='/construction.png' alt='Página en construcción' width={0}
+                   height={0}
+                   sizes="50vw"
+                   className="w-full h-auto" />
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <main>
+                <h1 className="text-4xl font-bold mb-48">Bienvenido a mi página Web</h1>
+                <h1 className="text-2xl font-bold mb-16">La idea de esta página es servir como carta de presentación.</h1>
+                <h1 className="text-2xl font-bold mb-16">En esta página vas a poder encontrar tanto mi formación educativa como
+                      proyectos de índole personal.</h1>
+                <h1 className="text-2xl font-bold mb-16">Se acepta todo tipo de comentario</h1>
+          </main>
+      </main>
+    </div>
+  );
+}
